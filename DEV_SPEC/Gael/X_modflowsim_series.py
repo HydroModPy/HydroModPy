19fc400663125995174ab8b82cbd5f149f79ce60
Gaël Chrétien <gael-de-sailly@netc.eu>
2021-04-23 12:05:08 +0200

    Parallelize modflowsim and modpathsim using MPI when multiple simulations are run, each of them takes one core

--- DEV_SPEC/Gael/X_modflowsim_series.py
+++ DEV_SPEC/Gael/X_modflowsim_series.py
@@ -5,6 +5,18 @@ import numpy as np
 import sys
 import os
 
+try:
+    from mpi4py import MPI
+    use_mpi = True
+except ImportError:
+    use_mpi = False
+
+if use_mpi:
+    comm = MPI.COMM_WORLD
+    r = comm.Get_rank()
+    s = comm.Get_size()
+    i = 0
+
 recharges = np.logspace(-10, -5, 16)
 
 for dem_path in sys.argv[1:]:
@@ -12,8 +24,13 @@ for dem_path in sys.argv[1:]:
     K = 1e-6
 
     for R, rech in enumerate(recharges, start=1):
+        if use_mpi:
+            i += 1
+            if i%s != r:
+                continue
         model_name = "R{:d}".format(R)
         print(watershed, model_name)
+        sys.stdout.flush()
         model = MfModel(dem_path, watershed=watershed, model_name=model_name, climatic=[rech], lay_number=1, bottom=-100., hyd_cond=K, exe='mfnwt')
 
         model.export_data()
--- DEV_SPEC/Gael/X_modpathsim_series.py
+++ DEV_SPEC/Gael/X_modpathsim_series.py
@@ -5,14 +5,31 @@ import numpy as np
 import sys
 import os
 
+try:
+    from mpi4py import MPI
+    use_mpi = True
+except ImportError:
+    use_mpi = False
+
+if use_mpi:
+    comm = MPI.COMM_WORLD
+    r = comm.Get_rank()
+    s = comm.Get_size()
+    i = 0
+
 recharges = np.logspace(-10, -5, 16)
 
 for watershed in sys.argv[1:]:
     K = 1e-6
 
     for R, rech in enumerate(recharges, start=1):
+        if use_mpi:
+            i += 1
+            if i%s != r:
+                continue
         model_name = "R{:d}".format(R)
         print(watershed, model_name)
+        sys.stdout.flush()
         model = MpModel(watershed=watershed, model_name=model_name, exe='mp6')
 
         model.export_data()
