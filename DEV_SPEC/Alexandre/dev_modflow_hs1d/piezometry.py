154f21777a86c7f83b3b2993d81be0adcff481d0
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-06-09 09:19:26 +0200

    Update piezometry.py

---
+++ DEV_SPEC/Alexandre/dev_modflow_hs1d/piezometry.py
@@ -0,0 +1,108 @@
+# coding:utf-8
+import os
+import urllib
+import zipfile
+import geopandas as gpd
+from selenium import webdriver
+import pandas as pd
+import time
+import glob
+
+class extract:
+	def __init__(self, out_path, geographic):
+		data_folder = out_path + 'data/piezometric/'
+		if not os.path.exists(data_folder):
+				os.makedirs(data_folder)
+				
+		#self.download_init_data(data_folder)
+		self.exctract_piezos_from_watershed(data_folder, geographic)
+		#self.extract_data_from_code_bss(data_folder)
+		self.load_piezometric_data(data_folder)
+
+	def download_init_data(self,data_folder):
+		filename = data_folder + 'piezometers.zip'
+		folder = data_folder + '/' + 'piezometers'
+		url = 'https://www.data.gouv.fr/fr/datasets/r/509bbb07-e375-45a2-81ac-c17820f57546'
+		if not os.path.exists(folder):
+			urllib.request.urlretrieve(url, filename)
+			with zipfile.ZipFile(filename, 'r') as zip_ref:
+				zip_ref.extractall(data_folder + '/' + 'piezometers')
+			os.remove(filename)
+		return
+
+	def exctract_piezos_from_watershed(self,data_folder, geographic):
+		watershed = gpd.read_file(geographic.watershed_box_shp)
+		piezos_shp = data_folder + 'piezometers/point_eau_piezo.shp'
+		piezos_fr = gpd.read_file(piezos_shp)
+		piezos_fr.to_crs(epsg=2154, inplace=True)
+		piezos = gpd.clip(piezos_fr, watershed)
+		self.codes_bss = piezos['code_bss'].tolist()
+		for i in range (0, len(self.codes_bss)):
+			self.codes_bss[i] = self.codes_bss[i].replace('/','_')
+		self.x_coord = piezos['geometry'].x.tolist()
+		self.y_coord = piezos['geometry'].y.tolist()
+
+	def extract_data_from_code_bss(self,data_folder):
+		for code in self.codes_bss:
+			code_ = code.replace('_','/')
+			url = 'https://ades.eaufrance.fr/Fiche/PtEau?Code=' + code_
+
+
+			chrome_options = webdriver.ChromeOptions()
+			prefs = {'download.default_directory' : data_folder.replace('/','\\')}
+			chrome_options.add_experimental_option('prefs', prefs)
+			driver = webdriver.Chrome(chrome_options=chrome_options)
+
+			driver.get(url)
+			elem = driver.find_element_by_link_text('Tout télécharger')
+			elem.click()
+			
+			compt = 0
+			while (compt==0):
+				if len(glob.glob(data_folder + '*.zip')) == 1:
+					compt +=1
+					time.sleep(1)
+				time.sleep(1)
+
+			driver.close()
+
+			file = glob.glob(data_folder+'/*.zip')[0]
+			with zipfile.ZipFile(file, 'r') as zip_ref:
+				zip_ref.extractall(data_folder+'/'+code)
+
+			os.remove(file)
+
+	def load_piezometric_data(self,data_folder):
+		self.depth = pd.DataFrame()
+		self.elevation = pd.DataFrame()
+		for code in self.codes_bss:
+			file = data_folder + code + '/ades_export/Quantite/chroniques.txt'
+			df = pd.read_csv(file, delimiter = '|',header=0, engine='python')
+
+			depth = df[['Date de la mesure','Profondeur relative/repère de mesure']]
+			depth.columns = ['Date', 'Mesure']
+			depth.index = pd.to_datetime(depth['Date'],format='%d/%m/%Y %H:%M:%S')
+			depth = depth.drop(['Date'], axis=1)
+			depth.columns = [code]
+			self.depth = pd.concat([self.depth, depth], axis=1).sort_index()
+
+			elevation = df[['Date de la mesure','Côte NGF']]
+			elevation.columns = ['Date', 'Mesure']
+			elevation.index = pd.to_datetime(elevation['Date'],format='%d/%m/%Y %H:%M:%S')
+			elevation = elevation.drop(['Date'], axis=1)
+			elevation.columns = [code]
+			self.elevation = pd.concat([self.elevation, elevation], axis=1).sort_index()
+
+			
+
+
+
+
+
+
+
+
+
+
+
+		
--- DEV_SPEC/Alexandre/dev_modflow_hs1d/watershed.py
+++ DEV_SPEC/Alexandre/dev_modflow_hs1d/watershed.py
@@ -6,10 +6,11 @@ import geopandas as gpd
 from osgeo import gdal, osr
 from shutil import copyfile
 import numpy as np
+import deepdish as dd
 from IPython.core.debugger import set_trace as st
 import geographic
-import climatic
-import 
+import geology
+import piezometry
 ### Method 1
 import whitebox
 wbt = whitebox.WhiteboxTools()
@@ -19,147 +20,51 @@ wbt.set_verbose_mode(False)
 # wbt = WhiteboxTools()
 
 
-class watershed:
-	def __init__(self, dem_path, outlet, snap_dist=150, buff_dist=1000,
-                 save_gis=True, box = False,
-                 tmp_path=os.path.dirname(os.path.dirname(__file__))+'\\tmp\\',
-                 out_path=os.path.dirname(os.path.dirname(__file__))+'\\output\\'):
+class build:
+	def __init__(self, watershed_name, dem_path, x_outlet, y_outlet, snap_dist=150, buff_dist=1000,
+                 out_path=os.path.dirname(os.path.dirname(__file__))+'\\output\\', load=True):
+
+		self.name = watershed_name
+		self.dem_path = dem_path
+		self.x_outlet = x_outlet
+		self.y_outlet = y_outlet
+		self.snap_dist = snap_dist
+		self.buff_dist = buff_dist
+		self.out_path = out_path
+		self.watershed_folder = out_path + '/' + watershed_name + '/'
 
 
-		try:
+		if not os.path.exists(self.watershed_folder):
+				os.makedirs(self.watershed_folder)
+
+		if load==True:
 			self.load_object()
-		except:
+		else:
 			self.create_object()
 
 
 	def load_object(self):
-		#chronicle
-		self.
-		#vectors
-		self.crs = 
-		self.xmin = 
-		self.xmax =
-		self.ymin =
-		self.centroid =
-		self.nrow = 
-		self.ncol = 
-		self.resolution =
-		#matrix
-		self.elevation =
-		self.watershed_mask = 
-
+		dict_object = dd.io.load(self.watershed_folder + 'object.h5')
+		self.geographic = dict_object['geographic']
 
 
 	def create_object(self):
-		self.geographic = () #2D
-		self.hillslope() #1D
-		self.climatic()
-		self.piezometric()
-		self.hydrometric() 
-		self.save_variables()
+		#STURCUTRE DATA
+		self.geographic = geographic.extract(dem_path=self.dem_path, x=self.x_outlet, y=self.y_outlet, snap_dist=self.snap_dist, buff_dist=self.buff_dist,
+                 out_path=self.watershed_folder) #2D
+		#self.hillslope = hillslope() #1D
+		#self.geology =  geology()
 
-		
+		#MODELING DATA
+		#self.climatic = climatic()
 
+		#FIELD DATA
+		self.piezometry = piezometry.extract(out_path=self.watershed_folder,geographic=self.geographic)
+		#self.hydrometry = hydrometry()
+		#self.geochemistry = geochemistry()
+		#self.save_variables()
 
-class extract_watershed:
-	def __init__(self, dem_path, name, outlet, snap_dist=150, buff_dist=1000,
-                 save_gis=True, box = False,
-                 tmp_path=os.path.dirname(os.path.dirname(__file__))+'\\tmp\\',
-                 out_path=os.path.dirname(os.path.dirname(__file__))+'\\output\\'):
-    
-		self.ws = os.getcwd()
-		self.dem_path = dem_path
-		self.name = name 
-		self.out_path = out_path
-		self.tmp_path = tmp_path
-		self.save_gis = save_gis
-		
-		self.outlet = outlet
-		self.snap_dist = snap_dist
-		self.buff_dist = buff_dist
-		self.bounding_box = box
-		self.fill = self.tmp_path + 'fill.tif'
-		self.direc = self.tmp_path + 'direc.tif'
-		self.acc = self.tmp_path + 'acc.tif'
-
-		if self.save_gis == True :
-			self.gis_path = self.out_path + self.name + '/gis/'
-		else:
-			self.gis_path = self.tmp_path
-
-		self.outlet_shp = self.gis_path + 'outlet.shp'
-		self.outlet_snap_shp = self.gis_path + 'outlet_snap.shp'
-		self.watershed = self.gis_path + 'watershed.tif'
-		self.watershed_shp = self.gis_path + 'watershed.shp'
-		self.watershed_box_shp = self.gis_path + 'watershed_box.shp'
-		self.watershed_contour_shp = self.gis_path + 'watershed_contour.shp'		
-		self.watershed_dem = self.gis_path + 'watershed_dem.tif'
-		self.watershed_fill = self.gis_path + 'watershed_fill.tif'
-		self.watershed_direc = self.gis_path + 'watershed_direc.tif'
-		self.buffer = self.gis_path + 'buff.shp'
-		self.watershed_buff_dem = self.gis_path + 'watershed_buff_dem.tif'
-		self.watershed_buff_fill = self.gis_path + 'watershed_buff_fill.tif'
-		self.watershed_buff_direc = self.gis_path + 'watershed_buff_direc.tif'
-
-		self.generate_watershed_dem()
-
-	def generate_watershed_dem(self):
-
-		if not os.path.exists(self.gis_path):
-				os.makedirs(self.gis_path)
-
-		self.dem = gdal.Open(self.dem_path)
-		proj = osr.SpatialReference(wkt=self.dem.GetProjection())
-		self.geodata = self.dem.GetGeoTransform()
-		dist = np.linspace(0,self.buff_dist,self.buff_dist+1)*np.abs(self.geodata[1])
-		self.buff_dist = dist[np.abs(dist-self.buff_dist).argmin()]
-		self.crs = 'EPSG:'+str(proj.GetAttrValue('AUTHORITY',1))
-		wbt.fill_depressions(self.dem_path, self.fill)
-		wbt.d8_pointer(self.fill, self.direc, esri_pntr=False)
-		wbt.d8_flow_accumulation(self.fill, self.acc, log=True)
-		df = self.outlet
-		df.columns = ['x','y']
-		#df.columns = ['id','x','y','snap']
-#		df.columns = ['local','site','x','y','snap','tif','rech','litho']
-		gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df['x'], df['y']), crs=self.crs)
-		gdf.to_file(self.outlet_shp)
-		wbt.snap_pour_points(self.outlet_shp, self.acc, self.outlet_snap_shp, self.snap_dist)
-		wbt.watershed(self.direc, self.outlet_snap_shp, self.watershed, esri_pntr=False)
-		wbt.raster_to_vector_polygons(self.watershed, self.watershed_shp)
-		wbt.polygons_to_lines(self.watershed_shp, self.watershed_contour_shp)
-		site_polyg = gpd.read_file(self.watershed_shp)
-		site_polyg.to_file(self.watershed_shp)
-		if self.bounding_box == False:
-			site_polyg['geometry'] = site_polyg.geometry.buffer(self.buff_dist)
-			site_polyg.to_file(self.buffer)
-			wbt.clip_raster_to_polygon(self.dem_path,self.buffer,self.watershed_buff_dem)
-			wbt.clip_raster_to_polygon(self.fill,self.buffer,self.watershed_buff_fill)
-			wbt.clip_raster_to_polygon(self.direc,self.buffer,self.watershed_buff_direc)
-
-
-		if self.bounding_box == True:
-			wbt.minimum_bounding_envelope(self.watershed_shp,self.watershed_box_shp, features=False)
-			site_polyg = gpd.read_file(self.watershed_box_shp)
-			site_polyg.to_file(self.watershed_box_shp)
-			site_polyg['geometry'] = site_polyg.geometry.buffer(self.buff_dist)
-			site_polyg.to_file(self.buffer)
-			wbt.minimum_bounding_envelope(self.buffer,self.buffer, features=False)
-			site_polyg = gpd.read_file(self.buffer)
-			site_polyg.to_file(self.buffer)
-			wbt.clip_raster_to_polygon(self.dem_path,self.buffer,self.watershed_buff_dem)
-			wbt.clip_raster_to_polygon(self.fill,self.buffer,self.watershed_buff_fill)
-			wbt.clip_raster_to_polygon(self.direc,self.buffer,self.watershed_buff_direc)
-
-# 		wbt.clip_raster_to_polygon(self.dem_path,self.watershed_shp,self.watershed_dem)
-		wbt.clip_raster_to_polygon(self.watershed_buff_dem, self.watershed_shp, self.watershed_dem,
-                                   maintain_dimensions=True)
-		wbt.clip_raster_to_polygon(self.fill,self.watershed_shp,self.watershed_fill)
-		wbt.clip_raster_to_polygon(self.direc,self.watershed_shp,self.watershed_direc)
-
-		return self, os.chdir(self.ws)
-	
-##### Notes
-# df = pd.DataFrame(np.reshape(np.asarray(self.outlet),(1,2)), columns=['x','y'])
-# wbt.multi_part_to_single_part(self.watershed_shp, self.watershed_shp, exclude_holes=True)
-# wbt.clean_vector(self.watershed_shp, self.watershed_shp)
-# wbt.dissolve(self.watershed_shp, self.watershed_shp, field=None)
+	def save_variables(self):
+		dict_object = {"geographic":self.geographic}
+		#if 'self.geographic' in locals():
+		dd.io.save(self.watershed_folder + 'object.h5', dict_object)
