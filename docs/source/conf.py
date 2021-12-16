e84be2f65417e8157c4bc445332f879485355c72
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-16 12:43:32 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../../simpleble/'))
-
+sys.path.insert(0, os.path.abspath('../../CORE_COMM/watershed/'))
 
 # -- Project information -----------------------------------------------------
 
