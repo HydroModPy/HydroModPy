1e0bc1f591e5ce4b29d1ebdb61660fb541332562
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-22 12:42:41 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,6 +48,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.githubpages',
+    'jupyter_sphinx.execute'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
