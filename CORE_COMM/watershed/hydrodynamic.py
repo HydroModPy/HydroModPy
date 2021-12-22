596ab11b9878e868391f9782caed1523ae63b6c4
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-22 23:38:54 +0100

    Update hydrodynamic.py

--- CORE_COMM/watershed/hydrodynamic.py
+++ CORE_COMM/watershed/hydrodynamic.py
@@ -18,7 +18,10 @@ class Hydrodynamic:
     :param ncol: number of columns in the DEM.
     :param hyd_cond_init: initial hydraulic conductivy of the aquifer. The default is 8.64.
     :param porosity_init: initial porosity of the aquifer. The default is 0.1.
-    :param thickness_init: initial thickness of the aquifer. The default is 50.    
+    :param thickness_init: initial thickness of the aquifer. The default is 50.
+    
+    :ivar hyd_cond: initial value: par1
+    :ivar porosity: initial value: par2
     """
     def __init__(self, nrow: int, ncol: int, hyd_cond_init: float = 8.64, porosity_init: float = 0.1, 
                  thickness_init: float = 50):
