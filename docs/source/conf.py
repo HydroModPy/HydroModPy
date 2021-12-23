3c68d3f05fb0fd5b24d5658bad1ee5860ed9308b
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-23 16:38:35 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,7 +89,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-
+html_favicon = 'logoHydroModPy.png'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
