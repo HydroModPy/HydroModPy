6430a8fcf6d6111b353c8ce2ae588be82b91335b
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-23 17:14:13 +0100

    conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -97,8 +97,20 @@ html_logo = 'logoHydroModPy_long.png'
 # documentation.
 #
 html_theme_options = {
+    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
+    'analytics_anonymize_ip': False,
     'logo_only': True,
     'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
