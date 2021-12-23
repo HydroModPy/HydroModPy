2c40f5b92050144f42adf04b050b17ce5ac255a5
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-23 17:30:26 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -103,7 +103,7 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'vcs_pageview_mode': '',
+    'vcs_pageview_mode': 'display_gitlab',
     'style_nav_header_background': '#343131',
     # Toc options
     'collapse_navigation': True,
