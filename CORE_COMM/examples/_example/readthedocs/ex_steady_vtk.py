c4ee299cdade176f384901443be0c0e057c7aa72
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2021-12-27 21:09:48 +0100

    readthedocs

--- CORE_COMM/examples/_example/readthedocs/ex_steady_vtk.py
+++
@@ -1,4 +0,0 @@
-from groundwater_flow import vizualisation
-vtk.VTK(BV, model_name)
-visu = vizualisation.Vizualisation(BV, model_name)
-visu.visual3D(interactive=false, object_list=['grid','watertable','watertable_depth'], view='south-west')
