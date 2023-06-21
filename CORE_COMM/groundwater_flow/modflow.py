5ed3361a5a84ce68d1fe75a1b8193ac5152219b1
ABHERVE Ronan <ronan.abherve@unine.ch>
2023-06-21 14:00:19 +0200

    Update bottom and lasset proj chyn

--- CORE_COMM/groundwater_flow/modflow.py
+++ CORE_COMM/groundwater_flow/modflow.py
@@ -480,10 +480,11 @@ class Modflow():
             self.bottom_layer = self.dem - self.thick    # Matrix for constant thickness case
         else:
             if isinstance(self.bottom,(int,float))==True:
-                bottom_layer = self.bottom              # Float for flat bottom case or 2D
-            if len(self.bottom.shape) == 2:
-                self.bottom_layer = self.bottom
-                self.bottom_layer[self.dem<=-9999]=-9999
+                self.bottom_layer = self.bottom              # Float for flat bottom case or 2D
+            else:
+                if len(self.bottom.shape) == 2:
+                    self.bottom_layer = self.bottom
+                    self.bottom_layer[self.dem<=-9999]=-9999
             # else isinstance(self.bottom,(int,float))==True:
             # elif len(self.bottom.shape) == 2:
         
