cb5736d9f14b348287c4484eaea7c2baef912a0f
Alexandre Gauvain <alexandre.gauvain.ag@gmail.com>
2023-07-28 13:27:43 +0200

    bash install and .yml

---
+++ install.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+# Install anaconda if  not installed
+if [ -d "$CONDA_PREFIX" ];
+then
+    echo "Conda is installed. Start to install HydroModPy environment"
+    install_conda='True'
+else
+	echo "Conda is not installed. Start to install Anaconda3"
+  install_conda='False'
+fi
+
+
+#Find conda.sh
+source $CONDA_PREFIX/etc/profile.d/conda.sh
+
+# Delete if already install
+conda deactivate
+conda remove --name hydromodpy-test --all -y
+
+# Build HydroModPy environment
+conda create -y --name hydromodpy-test python=3.8.10
+
+# Activate HydroModPy environment
+conda activate hydromodpy-test
+
+# Check Python version
+python --version
+
+# Add necessary librairies
+echo 'Install gdal'
+conda install -c conda-forge gdal=3.0.2 -y
+echo 'Install rasterio'
+pip install rasterio==1.2.10 --quiet #conda install -c conda-forge rasterio=1.2.10
+echo 'Install numpy'
+pip install numpy==1.24.3 --quiet
+echo 'Install pandas'
+pip install pandas==1.5.3 --quiet
+echo 'Install geopandas'
+pip install geopandas==0.12.2 --quiet # geopandas install matplotlib
+# pip install matplotlib==3.7.1 
+echo 'Install deepdish'
+pip install deepdish==0.3.7 --quiet
+echo 'Install flopy'
+pip install flopy==3.3.4 --quiet
+echo 'Install imageio'
+pip install imageio==2.31.1 --quiet
+echo 'Install whitebox'
+pip install whitebox==2.3.1 --quiet
+echo 'Install vedo'
+pip install vedo==2023.4.6 --quiet
+echo 'Install hydroeval'
+pip install hydroeval==0.1.0 --quiet
+echo 'Install xarray'
+pip install xarray==2023.1.0 --quiet
+echo 'Install netCDF4'
+pip install netCDF4==1.6.4 --quiet
+echo 'Install matplotlib_scalebar'
+pip install matplotlib_scalebar==0.8.1 --quiet
+echo 'Install contextily'
+pip install contextily==1.3.0 --quiet
+echo 'Install pyproj'
+pip install pyproj==3.5.0 --quiet
+echo 'Install selenium'
+pip install selenium==4.10.0 --quiet
+echo 'Install pyshp'
+pip install pyshp==2.3.1 --quiet
+echo 'Install jupyter'
+pip install jupyter==1.0.0 --quiet
+echo 'Install notebook'
+pip install notebook --quiet
+echo 'Install spyder'
+pip install spyder==5.0.0 --quiet
+echo 'Install rtree'
+pip install 'rtree>=0.8.3' --quiet
+
+echo ''
+
+echo 'HydroModPy is installed: Please check if all librairies are completely installed with this command: conda list'
+
+echo '+-------------------------------------------------------------+'
+echo '| #  #  #  #  ###   ###    ##   #   #   ##   ###   ###   #  # |'
+echo '| #  #   # #  #  #  #  #  #  #  ## ##  #  #  #  #  #  #   # # |'
+echo '| ####    #   #  #  ###   #  #  # # #  #  #  #  #  ###     #  |'
+echo '| #  #   #    #  #  # #   #  #  #   #  #  #  #  #  #      #   |' 
+echo '| #  #  #     ###   #  #   ##   #   #   ##   ###   #     #    |'
+echo '+-------------------------------------------------------------+'
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+import sys, platform
+import setuptools
+
+with open("README.md", "r") as file:
+    long_desc = file.read()
+
+setuptools.setup(
+    name='HydroModPy',
+    version='1.0',
+    author="Alexandre Gauvain, Ronan Abhervé",
+    author_email="alexandre.gauvain.ag@gmail.com",
+    description="Hydrological model at catchment scale",
+    long_description=long_desc,
+    long_description_content_type='text/markdown',
+    url = "https://gitlab.com/Alex-Gauvain/HydroModPy",
+    install_requires=[
+                      'numpy==1.24.3',
+                      'pandas==1.5.3',
+                      'geopandas==0.12.2',
+                      'matplotlib==3.7.1',
+                      'GDAL==3.0.2',
+                      'rasterio==1.2.10',
+                      'deepdish==0.3.7',
+                      'flopy==3.3.4',
+                      'imageio==2.31.1',
+                      'whitebox==2.3.1',
+                      'vedo==2023.4.6',
+                      'hydroeval==0.1.0',
+                      'xarray==2023.1.0',
+                      'netCDF4==1.6.4',
+                      'matplotlib_scalebar==0.8.1',
+                      'contextily==1.3.0',
+                      'pyproj==3.5.0',
+                      'selenium==4.10.0',
+                      'pyshp==2.3.1',
+                      'jupyter==1.0.0',
+                      'spyder',
+                      'notebook'],
+    python_requires='==3.8.10',
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    #data_files=[("lib\\site-packages\\ArchPy\\libraries", ["ArchPy\\libraries\\cov_facies.dll"])],
+    license=open('LICENSE', encoding='utf-8').read()
+)
