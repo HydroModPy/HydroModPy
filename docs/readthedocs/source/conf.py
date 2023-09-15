174a3f42f64d231feb55fd75b8585eb3b21641c6
Alexandre Gauvain <alexandre.gauvain.ag@gmail.com>
2023-09-15 10:27:10 +0200

    readthedocs

--- docs/readthedocs/source/conf.py
+++ docs/readthedocs/source/conf.py
@@ -170,7 +170,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'HydroModPy.tex', 'HydroModPy Documentation',
-     ", ".join(author), ", ".join(author), 'report'),
+     [", ".join(author)], ", ".join(author), 'report'),
 ]
 
 latex_logo = 'images/logoHydroModPy_long.png'
