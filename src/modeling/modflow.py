932cd4501b7e45f17455fa0a63bfcb50800ca21f
AlexCoche <alexandre.coche@univ-rennes1.fr>
2024-02-06 23:22:14 +0100

    Correct ibound on lakes

--- src/modeling/modflow.py
+++ src/modeling/modflow.py
@@ -367,6 +367,10 @@ class Modflow:
         # iboundData=-1: Values imposed at the value of strtData
         self.iboundData = np.ones((self.nlay, self.nrow, self.ncol))
         
+        # Correct ibound in lakes
+        if self.lakeres and self.lakeres.n_lakeres > 0:
+            self.iboundData[lakarr != 0] = 0
+        
         # Free surface level is set to the surface (altitude of DEM)
         self.strtData = np.ones((self.nlay, self.nrow, self.ncol))* self.dem   
         
