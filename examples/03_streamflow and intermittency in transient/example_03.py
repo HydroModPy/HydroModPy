598aacb404435aa1e63f73107a6e078fd9f89be1
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2024-02-27 15:59:06 +0100

    update modflow.py

--- examples/03_streamflow and intermittency in transient/example_03.py
+++ examples/03_streamflow and intermittency in transient/example_03.py
@@ -69,7 +69,7 @@ example_path = root_dir + "/examples/03_streamflow and intermittency in transien
 data_path = example_path + "data/"
 # To change the folder path: out_path = os.path.join(folder_root.update_root_folder_results(), 'EXHMP01')
 # out_path = os.path.join(folder_root.root_folder_results(), 'EXHMP03')
-out_path = 'C:/Users/ronan/Local/SIMULATIONS/HYDROMODPY/'
+out_path = '/home/agauvain/Documents/HydroModPy'
 
 #%% ---- WATERSHED
 
@@ -317,7 +317,7 @@ axs = axs.ravel()
 
 for i, simul in enumerate(simul_list[:]):
     
-    model_name = simul.split('\\')[-1]
+    model_name = simul.split('/')[-1]
     
     ax = axs[i]
 
@@ -426,7 +426,7 @@ simul_list = sorted(glob.glob(simulations_folder+iD_set_simulations+'*'),
         
 for simul in simul_list[:]:
     
-    model_name = simul.split('\\')[-1]
+    model_name = simul.split('/')[-1]
     Smod_path = simul+'/_postprocess/_timeseries/_simulated_timeseries.csv'
     Smod = pd.read_csv(Smod_path, sep=';', index_col=0, parse_dates=True)
         
@@ -515,7 +515,7 @@ axs = axs = axs.ravel()
 
 for i, simul in enumerate(simul_list[:]):
     
-    model_name = simul.split('\\')[-1]
+    model_name = simul.split('/')[-1]
 
     ax = axs[i]
     
@@ -567,7 +567,7 @@ for i, simul in enumerate(simul_list[:]):
     fig, (a0, a1) = plt.subplots(1, 2, gridspec_kw={'width_ratios': [3, 1]},
                                  figsize=(10,3))
     
-    model_name = simul.split('\\')[-1]
+    model_name = simul.split('/')[-1]
     
     Smod_path = simul+'/_postprocess/_timeseries/_simulated_timeseries.csv'
     Smod = pd.read_csv(Smod_path, sep=';', index_col=0, parse_dates=True)
@@ -656,7 +656,7 @@ simul_list = sorted(glob.glob(simulations_folder+iD_set_simulations+'*'),
 
 for i, simul in enumerate(simul_list[:]):
     
-    model_name = simul.split('\\')[-1]
+    model_name = simul.split('/')[-1]
     
     Smod_path = simul+'/_postprocess/_timeseries/_simulated_timeseries.csv'
     Smod = pd.read_csv(Smod_path, sep=';', index_col=0, parse_dates=True)
