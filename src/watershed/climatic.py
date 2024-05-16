2660a52bc06f561e35e6bc27969158f3aa20c20a
AlexCoche <alexandre.coche@univ-rennes.fr>
2024-05-16 20:22:32 +0200

    Forward-fill NaN values in recharge and runoff input chronicles
    Usefull when upsampling (for instance weekly data upsampled to daily data).

--- src/watershed/climatic.py
+++ src/watershed/climatic.py
@@ -210,6 +210,7 @@ class Climatic:
         self.recharge = climatic # recharge in meters
         # self.recharge.index = self.recharge.asfreq(self.freq).index
         self.recharge = self.recharge.resample(self.freq).mean()
+        self.recharge.fillna(method = 'ffill', inplace = True)
         # self.recharge.index = self.recharge.index.to_period(self.freq)
         if sim_state == 'steady':
             self.recharge = self.recharge.mean()
@@ -241,6 +242,7 @@ class Climatic:
         self.runoff = climatic # recharge in meters
         # self.runoff.index = self.runoff.asfreq(self.freq).index
         self.runoff = self.runoff.resample(self.freq).mean()
+        self.runoff.fillna(method = 'ffill', inplace = True)
         # self.runoff.index = self.runoff.index.to_period(self.freq)
         if sim_state == 'steady':
             self.runoff = self.runoff.mean()
@@ -282,6 +284,7 @@ class Climatic:
         self.recharge = climatic/1000 # recharge in meters
         # self.recharge.index = self.recharge.asfreq(self.freq).index
         self.recharge = self.recharge.resample(self.freq).mean()
+        self.recharge.fillna(method = 'ffill', inplace = True)
         # self.recharge.index = self.recharge.index.to_period(self.freq)
         if sim_state == 'steady':
             self.recharge = self.recharge.mean()
@@ -315,6 +318,7 @@ class Climatic:
         self.runoff = climatic/1000 # recharge in meters
         # self.runoff.index = self.runoff.asfreq(self.freq).index
         self.runoff = self.runoff.resample(self.freq).mean()
+        self.runoff.fillna(method = 'ffill', inplace = True)
         # self.runoff.index = self.runoff.index.to_period(self.freq)
         if sim_state == 'steady':
             self.runoff = self.runoff.mean()
