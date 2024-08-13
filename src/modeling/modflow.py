0f12284c591f61ad5d67e0ce9b3fd1c57ae53002
AlexCoche <alexandre.coche@univ-rennes.fr>
2024-08-13 23:51:12 +0200

    Improve parameter values
    - for better convergence and less leakage

--- src/modeling/modflow.py
+++ src/modeling/modflow.py
@@ -868,14 +868,14 @@ class Modflow:
 # =============================================================================
 
         # 1. Standard values on segment_data_1:
-        depth = 1 # arbitrary
+        depth = 0 # 0.1 # self.thick # 1 # arbitrary
         self.segment_data_1['thickm1'] = 0.1 # Modflow does not run if = 0
         self.segment_data_1['thickm2'] = 0.1
         self.segment_data_1['depth1'] = depth
         self.segment_data_1['depth2'] = depth
         self.segment_data_1['icalc'] = icalc 
-        self.segment_data_1['hcond1'] = 0.0008 # 3e-5 # self.hyd_cond[0, 0] # 864000
-        self.segment_data_1['hcond2'] = 0.0008 # 3e-5 # self.hyd_cond[0, 0] # 864000
+        self.segment_data_1['hcond1'] = 0.002 # 3e-5 # self.hyd_cond[0, 0] # 864000
+        self.segment_data_1['hcond2'] = 0.002 # 3e-5 # self.hyd_cond[0, 0] # 864000
         self.segment_data_1['width1'] = 1 # self.resolution # 1.5  # arbitrary
         self.segment_data_1['width2'] = 1 # self.resolution # 1.5 
 
@@ -895,7 +895,9 @@ class Modflow:
         # 3. Update values on reach_data:
         self.reach_data['k'] = 0 # layer
         for idx, r in self.reach_data.iterrows(): # strtop
-            self.reach_data.loc[idx, 'strtop'] = self.dem[r['i'], r['j']] - depth
+            self.reach_data.loc[idx, 'strtop'] = self.dem[r['i'], r['j']]
+                                               # self.dem[r['i'], r['j']] - depth
+                                               # self.bottom_layer[r['i'], r['j']] 
         rchlen = self.geographic.resolution * (1/4 + 1/(2*np.sqrt(2))) # average straight euclidien length
         self.reach_data['rchlen'] = rchlen # rchlen:
 
