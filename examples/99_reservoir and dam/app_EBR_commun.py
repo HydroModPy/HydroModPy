af17989d038a617918b71fa2fca59fe78ef5d8a5
AlexCoche <alexandre.coche@univ-rennes.fr>
2024-08-30 20:02:38 +0200

    Minor simplifications in app_EBR_commun (for testing)
    - less climatic data downloading
    - commenting 'Input flow' section

--- examples/99_reservoir and dam/app_EBR_commun.py
+++ examples/99_reservoir and dam/app_EBR_commun.py
@@ -150,7 +150,8 @@ freq_input = 'W' # hebdomadaire
 
 ##%%% Reanalyse
 BV.climatic.update_sim2_reanalysis(var_list=['recharge', 'runoff', 
-                                              'evt', 'etp', 'precip', 'temp'],
+                                               # 'evt', 'etp', 'precip', 'temp',
+                                              ],
                                        nc_data_path=os.path.join(
                                            data_path,
                                            r"Meteo"),
@@ -664,26 +665,33 @@ BV.save_object()
 
 
 #%%% (Input flow)
-# (used here to force a return flow)
-# Return flow time series
-return_flow_series = dam_input_df['resti']
-# return_flow_series can also be a .txt file
-
-# Coordinates of the cell where the return flow is mesured
-return_flow_coords = (331500, 6781425) # tuple or list of tuples 
 # =============================================================================
-# fixed_flow_coords = os.path.join(root_dir, 'examples', '99_reservoir and dam',
-#                                  'data', 'additional', 'coords_forcedflow.txt')
+# # (used here to force a return flow)
+# # Return flow time series
+# return_flow_series = dam_input_df['resti']
+# # return_flow_series can also be a .txt file
+# 
+# # Coordinates of the cell where the return flow is mesured
+# return_flow_coords = (331500, 6781425) # tuple or list of tuples 
+# # =============================================================================
+# # fixed_flow_coords = os.path.join(root_dir, 'examples', '99_reservoir and dam',
+# #                                  'data', 'additional', 'coords_forcedflow.txt')
+# # =============================================================================
+#                     # the coords can also be indicated as a .txt file
+# 
+# bound_id = 0 # identifier for the cell (or cells) where the return flow will be forced
+# snap_dist = 200
+# BV.settings.add_inputflow(bound_id, return_flow_coords, snap_dist,
+#                           return_flow_series)
+# 
+# # To remove a forced-flow cell or group of cells:
+# # BV.lakeres.remove_flowbound(bound_id)
 # =============================================================================
-                    # the coords can also be indicated as a .txt file
 
-bound_id = 0 # identifier for the cell (or cells) where the return flow will be forced
-snap_dist = 200
-BV.settings.add_inputflow(bound_id, return_flow_coords, snap_dist,
-                          return_flow_series)
+# =============================================================================
+# =============================================================================
 
-# To remove a forced-flow cell or group of cells:
-# BV.lakeres.remove_flowbound(bound_id)
+
 
 
 #%% PARAMETRISATION
