ccb5f81c4867971b462736c463394ac90e4adfa0
AlexCoche <alexandre.coche@univ-rennes.fr>
2024-09-16 19:40:46 +0200

    Safeguard against rasterio and proj incomptability {to check}
    - rasterio installs proj as a wheel
    - if proj is already installed, this wil create a conflict as rasterio will not use the right folder
    - to avoid that, PROJ_LIB needs to be removed from environment variables (needs to be done before running the launcher)

--- users/EBR/app_EBR_commun.py
+++ users/EBR/app_EBR_commun.py
@@ -44,6 +44,8 @@ import imageio
 import whitebox
 wbt = whitebox.WhiteboxTools()
 wbt.verbose = False
+if os.getenv('PROJ_LIB') is not None:
+    os.environ.pop('PROJ_LIB')
 import xarray as xr
 xr.set_options(keep_attrs = True)
 
