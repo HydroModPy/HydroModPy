c7af723c3eeb960f347d08d7f9e9343f1ca731e2
AlexCoche <alexandre.coche@univ-rennes.fr>
2024-10-17 19:40:53 +0200

    Major correction on bathymetry (2/3)
    - it is necessary to properly update the original DEM (not overwrite original values with nodata from, especially in the case of no-box dem)

--- src/watershed/lakeres.py
+++ src/watershed/lakeres.py
@@ -702,6 +702,9 @@ class Lakeres:
         with rio.open(geographic.dem_path, 'r+') as bathy_dem:
             with rio.open(geographic.watershed_box_buff_dem, 'r') as box:
                 window = rio.windows.from_bounds(*box.bounds, transform=bathy_dem.transform)
+                # Update original DEM data
+                val = bathy_dem.read(1, window=window)
+                val = np.where(dem != geographic.nodata, dem, val) 
                 bathy_dem.write_band(1, dem, window=window)
         
         # Repeat the generation of geographic maps
