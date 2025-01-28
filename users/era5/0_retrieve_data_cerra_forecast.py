37c3e87f4313a2c4cfcdf78bd7c01e94e9b9e263
roquesclem <roquesclem@gmail.com>
2025-01-28 13:46:38 +0100

    Update ERA5

--- users/era5/0_retrieve_data_cerra_forecast.py
+++ users/era5/0_retrieve_data_cerra_forecast.py
@@ -78,7 +78,7 @@ selected_variables = [
     'total_precipitation',
     'surface_net_solar_radiation']
 
-start = 1985
+start = 1984
 stop = 2022
 years = np.linspace(start, stop, stop - start + 1).astype(int)
 months = np.linspace(1, 12, 12).astype(int)
@@ -100,46 +100,59 @@ for v in selected_variables:
     
     for y in years:
         year_to_retrieve = str(y)
-
-        for m in months:
-            month_to_retrieve = str(m)
-            print(f"**Year {year_to_retrieve}\n**Month {month_to_retrieve}\n")
+        print(f"year: {year_to_retrieve}\n")
+        folder_to_save = os.path.join(main_folder, today, variable_to_retrieve, year_to_retrieve)
+        os.makedirs(folder_to_save, exist_ok=True)
+        
+        name_to_save = os.path.join(folder_to_save, f"{year_to_retrieve}.nc")
+        start_time = time.time()
+        dataset = "reanalysis-cerra-single-levels"
+        request = {
+            "level_type": "surface_or_atmosphere",
+            "data_type": ["reanalysis"],
+            "product_type": "forecast",
+            "variable": variable_to_retrieve,
+            "year": year_to_retrieve,
+            "month": [
+                "01", "02", "03",
+                "04", "05", "06",
+                "07", "08", "09",
+                "10", "11", "12"
+            ],
+            "day": [
+                "01", "02", "03",
+                "04", "05", "06",
+                "07", "08", "09",
+                "10", "11", "12",
+                "13", "14", "15",
+                "16", "17", "18",
+                "19", "20", "21",
+                "22", "23", "24",
+                "25", "26", "27",
+                "28", "29", "30",
+                "31"
+            ],
+            "time": [
+                "00:00", "03:00", "06:00",
+                "09:00", "12:00", "15:00",
+                "18:00", "21:00"
+            ],
+            "leadtime_hour": ["6"],
+            "data_format": "netcdf",
+        }
+        
+        try:
+            client.retrieve(dataset, request, name_to_save)
+            end_time = time.time()
+            duration = end_time - start_time
+            print(f"Time taken for {variable_to_retrieve} for {year_to_retrieve}: {duration:.2f} seconds\n")
+        except Exception as e:
+            error_message = str(e)
+            print(f"Failed to download {variable_to_retrieve} for {year_to_retrieve}: {error_message}\n")
             
-            folder_to_save = os.path.join(main_folder, today, variable_to_retrieve, year_to_retrieve)
-            os.makedirs(folder_to_save, exist_ok=True)
-            
-            name_to_save = os.path.join(folder_to_save, f"{month_to_retrieve}.nc")
-            start_time = time.time()
-            dataset = "reanalysis-cerra-single-levels"
-            request = {
-                "level_type": "surface_or_atmosphere",
-                "data_type": ["reanalysis"],
-                "product_type": "forecast",
-                "variable": variable_to_retrieve,
-                "year": year_to_retrieve,
-                "month": month_to_retrieve,
-                "day": [f"{day:02d}" for day in range(1, 32)],
-                "time": [
-                    "00:00", "03:00", "06:00",
-                    "09:00", "12:00", "15:00",
-                    "18:00", "21:00"
-                ],
-                "leadtime_hour": ["6"],
-                "data_format": "netcdf",
-            }
-            
-            try:
-                client.retrieve(dataset, request, name_to_save)
-                end_time = time.time()
-                duration = end_time - start_time
-                print(f"Time taken for {variable_to_retrieve} for {year_to_retrieve}-{month_to_retrieve}: {duration:.2f} seconds\n")
-            except Exception as e:
-                error_message = str(e)
-                print(f"Failed to download {variable_to_retrieve} for {year_to_retrieve}-{month_to_retrieve}: {error_message}\n")
-                
-                # Log the failure
-                with open(failure_log_file, mode='a', newline='') as file:
-                    writer = csv.writer(file)
-                    writer.writerow([variable_to_retrieve, year_to_retrieve, month_to_retrieve, error_message])
-                continue
-            print('#########################')
+            # Log the failure
+            with open(failure_log_file, mode='a', newline='') as file:
+                writer = csv.writer(file)
+                writer.writerow([variable_to_retrieve, year_to_retrieve, error_message])
+            continue
+        print('#########################')
