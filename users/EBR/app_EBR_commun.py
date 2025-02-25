52833b24ad651591ee6917c43fe45fd692b77d09
Alexandre Coche <alexandre.co@hotmail.fr>
2025-02-25 22:59:24 +0100

    Revert "[lakeres] Modify connect_returnflow"
    This reverts commit aea9869e2e56f29fdb2f94300649d1a23f372aba.

--- users/EBR/app_EBR_commun.py
+++ users/EBR/app_EBR_commun.py
@@ -710,7 +710,7 @@ for i in range(0, resti_short.size - 1):
     id2 = resti_short.index[i+1]
     resti_short.loc[id1] = dam_input_df[id1:id2][0:-1]['resti'].mean()
 resti_short = resti_short[0:-1]
-BV.lakeres.update_returnflow(lake_id, resti_short, mode = 'replace')
+BV.lakeres.connect_returnflow(lake_id, resti_short)
 
 ######################
 ### --- others --- ###
