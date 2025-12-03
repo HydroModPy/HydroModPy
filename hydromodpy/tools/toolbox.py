8277e37c17b08cf9ca5d4335104d6750055560d3
bastien-boivin <bastien.boivin@univ-rennes.fr>
2025-12-03 01:17:20 +0100

    [refactor] - Clean up imports

--- hydromodpy/tools/toolbox.py
+++ hydromodpy/tools/toolbox.py
@@ -14,7 +14,6 @@
 
 import os
 import re
-import math
 import numbers
 import datetime
 import matplotlib.pyplot as plt
@@ -33,7 +32,6 @@ from pyproj import Transformer
 from pyproj.aoi import AreaOfInterest
 from pyproj.database import query_utm_crs_info
 import pandas as pd
-from affine import Affine
 import numpy as np
 import whitebox
 wbt = whitebox.WhiteboxTools()
@@ -56,7 +54,7 @@ def create_folder(path):
     """
     if not os.path.exists(path):
         os.makedirs(path)
-        
+
 #%% RASTER PROCESSING
 
 def clip_tif(tif_path, shp_path, out_path, maintain_dimensions):
@@ -114,31 +112,31 @@ def load_to_numpy(file, src_crs=None,
                   base_path:str=None, dst_crs=None, out_path:str=None):
     """
     Generate a numpy array from a source file (vector or raster) and a base
-    raster. The numpy array profile (shape, resolution, extent...) matches 
+    raster. The numpy array profile (shape, resolution, extent...) matches
     with the base one.
     If the base raster is not specified (base_path), then the generated numpy
     array has the same profile as the source file.
-    
+
     When the source CRS is not embeded in the source file, it can be specified
     with src_crs.
     When the destination CRS is not embeded in the base file, it can also be
     specified with dst_crs.
-    
+
     out_path gives the possibility to export the result as a .tif file.
-    
+
 
     Parameters
     ----------
     file : str or geopandas.GeoDataFrame
         Path to the input file to process, or geopandas GoDataFrame.
     src_crs : int or str, optional (The default is None)
-        If the CRS is not embeded in the input file, it is possible to 
+        If the CRS is not embeded in the input file, it is possible to
         specify it here, as an integer (EPSG), or a str 'EPSG:<int>'
     base_path : str, optional (The default is None)
         Path to the file that will serve as the base for dimensions, resolution,
-        extent... 
+        extent...
     dst_crs : int or str, optional (The default is None)
-        If the CRS is not embeded in the base file, it is possible to 
+        If the CRS is not embeded in the base file, it is possible to
         specify it here, as an integer (EPSG), or a str 'EPSG:<int>'
     out_path : str, optional (The default is None)
         If specified, the numpy array will be saved as a .tif file, using the
@@ -151,7 +149,7 @@ def load_to_numpy(file, src_crs=None,
     """
     # Initializations:
     if base_path:
-        with rio.open(base_path, 'r') as base: 
+        with rio.open(base_path, 'r') as base:
             base_profile = base.profile
             base_val = base.read(1) # base.read()[0]
     else:
@@ -170,7 +168,7 @@ def load_to_numpy(file, src_crs=None,
         """
         The input file should be formated as:
             id;x;y
-            0;34500;7456125  
+            0;34500;7456125
             1;35675;7991500
             ...
         """
@@ -185,23 +183,23 @@ def load_to_numpy(file, src_crs=None,
                 file,
             )
             logger.debug("Coordinate CSV parsing error: %s", exc)
-    
+
     if file_vect is not None: # shapefile
         if base_profile:
             # CRS initialization
             if not file_vect.crs: # if not file_vect.crs.is_geographic nor file_vect.crs.is_projected:
-                if src_crs: 
+                if src_crs:
                     file_vect.set_crs(crs = src_crs, inplace = True, allow_override = True)
-                else: 
+                else:
                     logger.error("Source CRS (src_crs) required to rasterize vector dataset")
                     return
-                    
+
             if not base_profile['crs'].is_valid:
                 if dst_crs: base_profile['crs'] = dst_crs
-                else: 
+                else:
                     logger.error("Destination CRS (dst_crs) required to rasterize vector dataset")
                     return
-                    
+
             # The vector needs to be in the same CRS as the base raster:
             logger.info(f"Before rasterization, the vector will be converted from 'EPSG:{file_vect.crs.to_epsg()}' into 'EPSG:{base_profile['crs'].to_epsg()}'.")
             file_vect.to_crs(crs = base_profile['crs'].to_epsg(), inplace = True)
@@ -217,7 +215,7 @@ def load_to_numpy(file, src_crs=None,
         else: # if there is no base_profile
             logger.error("Raster profile required to rasterize vector dataset; provide base raster or profile")
             return
-    
+
     else: # input file is a raster
         with rio.open(file, 'r') as data:
             data_profile = data.profile
@@ -226,14 +224,14 @@ def load_to_numpy(file, src_crs=None,
                 # print(f"The CRS of input data has been set to 'EPSG:{data_profile['crs'].to_epsg()}'.\n")
             # data_crs = data.crs
             val = data.read(1) # data.read()[0] # extract the first layer
-    
+
     # Reprojection:
-    # if (crs_proj and (str(data_crs) != crs_proj)) or (base_profile and (data_profile != base_profile)):    
+    # if (crs_proj and (str(data_crs) != crs_proj)) or (base_profile and (data_profile != base_profile)):
     if base_profile:
         # CRS initialization
         if dst_crs and not base_profile['crs'].is_valid:
             base_profile['crs'] = dst_crs
-                
+
         if data_profile != base_profile:
             if not data_profile['crs'].is_valid:
                 logger.error("Source CRS (src_crs) required to reproject raster dataset")
@@ -241,8 +239,8 @@ def load_to_numpy(file, src_crs=None,
             if not base_profile['crs'].is_valid:
                 logger.error("Destination CRS (dst_crs) required to reproject raster dataset")
                 return
-            rio.warp.reproject(source = val, 
-                               destination = base_val, 
+            rio.warp.reproject(source = val,
+                               destination = base_val,
                                src_transform = data_profile['transform'],
                                src_crs = data_profile['crs'],
                                src_nodata = data_profile['nodata'],
@@ -256,9 +254,9 @@ def load_to_numpy(file, src_crs=None,
             data_profile = base_profile
             # update values array
             val = base_val
-        
-    
-    
+
+
+
     # Ne fonctionne pas encore
 # =============================================================================
 #     # Drop nodata margins:
@@ -280,27 +278,27 @@ def load_to_numpy(file, src_crs=None,
 # =============================================================================
 
     if out_path: # to export as a .tif file (optional)
-        with rio.open(out_path, 'w', **data_profile) as dst: 
+        with rio.open(out_path, 'w', **data_profile) as dst:
             dst.write_band(1, val)
-    
-    if base_profile:        
+
+    if base_profile:
         dst_crs = base_profile['crs']
         nodata = base_profile['nodata']
     else:
         nodata = None
-    
+
     if file_vect is not None:
         src_crs = file_vect.crs
     else:
         src_crs = data_profile['crs']
-    
+
     return val, src_crs, dst_crs, nodata
 
 
-def load_to_xarray(file, src_crs=None, main_var=None, 
+def load_to_xarray(file, src_crs=None, main_var=None,
                    base_path:str=None, dst_crs=None):
     """
-    
+
 
     Parameters
     ----------
@@ -327,10 +325,10 @@ def load_to_xarray(file, src_crs=None, main_var=None,
         DESCRIPTION.
 
     """
-    
+
     # ---- Initialization
     if base_path:
-        with rio.open(base_path, 'r') as base: 
+        with rio.open(base_path, 'r') as base:
             base_profile = base.profile
             base_val = base.read(1) # base.read()[0]
     else:
@@ -339,7 +337,7 @@ def load_to_xarray(file, src_crs=None, main_var=None,
     elif isinstance(src_crs, int): src_crs = rio.crs.CRS.from_epsg(src_crs)
     if isinstance(dst_crs, str): dst_crs = rio.crs.CRS.from_string(dst_crs)
     elif isinstance(dst_crs, int): dst_crs = rio.crs.CRS.from_epsg(dst_crs)
-    
+
     # ---- Loading netcdf
     if isinstance(file, str):
         if os.path.splitext(file)[-1].casefold() in ['.tif', '.tiff']:
@@ -349,23 +347,23 @@ def load_to_xarray(file, src_crs=None, main_var=None,
             ds = ds.drop('band')
             if main_var:
                 ds = ds.rename(dict(band_data = main_var))
-            
+
         elif os.path.splitext(file)[-1].casefold() == '.nc':
             try:
                 with xr.open_dataset(file, decode_coords = 'all') as ds:
                     ds.load() # to unlock the resource
-                    
-            except ValueError: 
-                # Usually this error appears when unable to decode 
-                # time units 'Months since 1901-01-01' with 
+
+            except ValueError:
+                # Usually this error appears when unable to decode
+                # time units 'Months since 1901-01-01' with
                 # "calendar 'proleptic_gregorian'"
                 logger.warning("Unable to decode NetCDF time units; falling back to manual parsing")
-                with xr.open_dataset(file, decode_coords = 'all', 
+                with xr.open_dataset(file, decode_coords = 'all',
                                      decode_times = False) as ds:
                     ds.load()
-                    
+
                 try: ds.time.attrs['units']
-                except: 
+                except:
                     logger.error("No time unit metadata found in NetCDF dataset")
                     return
                 # Build back time scale:
@@ -377,38 +375,38 @@ def load_to_xarray(file, src_crs=None, main_var=None,
                 elif timeunit in ['day', 'days', 'jour', 'jours']:
                     freq = '1D'
                     freq_info = 'daily'
-                
+
                 logger.info(
                     "Expecting origin date formatted as YYYY MM DD or DD MM YYYY when rebuilding time axis"
                 )
-                # The format of the origin date is expected to be either 
+                # The format of the origin date is expected to be either
                 # YYYY MM DD or DD MM YYYY (with any separator)
                 # The american format MM DD YYYY is not considered
                 initdate_pattern = re.compile(r"\d{2,4}.*\d{2,4}")
                 initdate = initdate_pattern.search(ds.time.attrs['units']).group()
-                
+
                 if initdate[2].isnumeric():
                     sep = initdate[4]
                     initdate = datetime.datetime.strptime(initdate, f"%Y{sep}%m{sep}%d")
                 else:
                     sep = initdate[2]
                     initdate = datetime.datetime.strptime(initdate, f"%d{sep}%m{sep}%Y")
-                
+
                 start_date = pd.Series(pd.date_range(
                     initdate, periods = int(ds.time[0]) + 1, freq = freq)).iloc[-1]
-                date_index = pd.date_range(start = start_date, 
-                                             periods = len(ds.time), freq = freq) 
+                date_index = pd.date_range(start = start_date,
+                                             periods = len(ds.time), freq = freq)
                 # print(f"Time axis from {date_index[0]} to {date_index[-1]} ({freq_info}).\n")
-                ds['time'] = date_index  
-        
+                ds['time'] = date_index
+
         else:
             logger.error("File extension %s not supported for xarray loading", os.path.splitext(file)[-1])
             return
-    
+
     elif isinstance(file, xr.core.dataset.Dataset):
         ds = file
-    
-    
+
+
     # ---- Reprojection
     # Add Coordinate Reference System if needed
     if not src_crs:
@@ -419,15 +417,15 @@ def load_to_xarray(file, src_crs=None, main_var=None,
         if not ds.rio.crs.is_valid:
             ds.rio.write_crs(src_crs, inplace = True)
         # print(f"The CRS of input data has been set to '{ds.rio.crs.to_string()}'.\n")
-    
+
     data_transform = ds.rio.transform()
-    
-    # if (crs_proj and (str(data_crs) != crs_proj)) or (base_profile and (data_profile != base_profile)):    
+
+    # if (crs_proj and (str(data_crs) != crs_proj)) or (base_profile and (data_profile != base_profile)):
     if base_profile:
         # CRS initialization
         if dst_crs and not base_profile['crs'].is_valid:
             base_profile['crs'] = dst_crs
-                
+
         if (data_transform != base_profile['transform']) | (ds.rio.crs != base_profile['crs']):
             if not ds.rio.crs.is_valid:
                 logger.error("Source CRS (src_crs) required to reproject xarray dataset")
@@ -443,12 +441,12 @@ def load_to_xarray(file, src_crs=None, main_var=None,
                                              resampling = rasterio.enums.Resampling(1), # (0), (5)
                                              )
             ds = ds_reprj
-    
+
     else:
         if dst_crs is not None:
             ds_reprj = ds.rio.reproject(dst_crs = dst_crs)
             ds = ds_reprj
-            
+
     # ---- Format spatial attributes for compatibility with QGIS
     if 'units' in ds.x.attrs.keys() and ds.x.attrs['units'].casefold() in ['m', 'meter', 'meters', 'metre', 'metres']:
         ds.x.attrs = {'standard_name': 'projection_x_coordinate',
@@ -462,10 +460,10 @@ def load_to_xarray(file, src_crs=None, main_var=None,
                               'units': 'degrees_east'}
         ds.latitude.attrs = {'long_name': 'latitude',
                              'units': 'degrees_north'}
-    
+
     return ds #, src_crs, dst_crs, nodata
 
-        
+
 #%% EXTRACTING FEATURES
 
 def basin_area(target_data, mask_data, cond_symb, value_masked, resolution):
@@ -588,11 +586,11 @@ def hydrological_mean(data, accuracy=15):
         The average value.
 
     """
-    
+
     #% Get rid of the first and last value (there are great chances that
     # they are irrelevant, especially in resampled data sets)
     data = data[1:-1]
-    
+
     #% Format the index to Timestamp, if needed
     if isinstance(data.index[0], numbers.Number):
         data.index = data['time']
@@ -602,17 +600,17 @@ def hydrological_mean(data, accuracy=15):
     if not isinstance(data.index[0], datetime.datetime):
         logger.error("No recognized datetime index in input series for hydrological_mean")
         return
-    
+
     #% Get the most recent date that falls within the accuracy range
     idx = data[data.index.month == data.index[0].month][
             abs(data[data.index.month == data.index[0].month].index.day - \
                 data.index[0].day)-3 <= 0].index[-1]
 
     # n_years = np.mean((data.index[-1]-data.index[0])/365.2425)
-    
+
     if (idx - data.index[0]).days < 350:
         logger.warning("Time range shorter than one year; using simple mean instead of hydrological mean")
-    
+
     # print(f"Average values are computed from {data.index[0].strftime('%Y-%m-%d')} to {idx.strftime('%Y-%m-%d')}")
 
     avg = data[data.index[0]:idx].mean(numeric_only = False)
@@ -625,7 +623,7 @@ def hydrological_mean(data, accuracy=15):
 def plot_params(small,interm,medium,large):
     """
     Change options for plots.
-    
+
     Parameters
     ----------
     small : float
@@ -646,7 +644,7 @@ def plot_params(small,interm,medium,large):
     interm = interm
     medium = medium
     large = large
-    
+
     # mpl.rcParams['backend'] = 'wxAgg'
     mpl.style.use('classic')
     mpl.rcParams["figure.facecolor"] = 'white'
@@ -660,7 +658,7 @@ def plot_params(small,interm,medium,large):
     mpl.rcParams['patch.force_edgecolor'] = True
     mpl.rcParams['image.interpolation'] = 'nearest'
     mpl.rcParams['image.resample'] = True
-    mpl.rcParams['axes.autolimit_mode'] = 'data' # 'round_numbers' # 
+    mpl.rcParams['axes.autolimit_mode'] = 'data' # 'round_numbers' #
     mpl.rcParams['axes.xmargin'] = 0.05
     mpl.rcParams['axes.ymargin'] = 0.05
     mpl.rcParams['xtick.direction'] = 'in'
@@ -685,7 +683,7 @@ def plot_params(small,interm,medium,large):
     mpl.rcParams['date.autoformatter.minute'] = '%H:%M:%S'
     mpl.rcParams['date.autoformatter.second'] = '%H:%M:%S'
     mpl.rcParams.update({'mathtext.default': 'regular' })
-    
+
     plt.rc('font', size=small)                         # controls default text sizes **font
     plt.rc('figure', titlesize=large)                   # fontsize of the figure title
     plt.rc('legend', fontsize=small)                     # legend fontsize
@@ -694,16 +692,16 @@ def plot_params(small,interm,medium,large):
     plt.rc('xtick', labelsize=interm)                   # fontsize of the tick labels
     plt.rc('ytick', labelsize=interm)                   # fontsize of the tick labels
     plt.rc('font', family='sans serif')
-    
+
     fontprop = FontProperties()
     fontprop.set_family('sans serif') # for x and y label
-    fontdic = {'family' : 'sans serif', 'weight' : 'bold'} # for legend  
+    fontdic = {'family' : 'sans serif', 'weight' : 'bold'} # for legend
 
-    return fontprop      
+    return fontprop
 
 #%% REPROJECT DATA
 
-def export_tif(base_dem_path, data_to_tif, data_tif_path, 
+def export_tif(base_dem_path, data_to_tif, data_tif_path,
                data_nodata_val=None, data_crs=None):
     """
     Export tif from 2D matrix data following raster reference.
@@ -734,16 +732,16 @@ def export_tif(base_dem_path, data_to_tif, data_tif_path,
     if data_nodata_val is not None:
         ras_meta['nodata'] = data_nodata_val
     if data_crs is not None:
-        if isinstance(data_crs, str): 
+        if isinstance(data_crs, str):
             ras_meta['crs'] = rio.crs.CRS.from_string(data_crs)
-        elif isinstance(data_crs, int): 
+        elif isinstance(data_crs, int):
             ras_meta['crs'] = rio.crs.CRS.from_epsg(data_crs)
         else:
             ras_meta['crs'] = data_crs
     # Create new data raster with base dem size
     with rio.open(data_tif_path, 'w', **ras_meta) as dst:
         dst.write(data_to_tif, 1)
-    
+
 def reproject_tif(raw_dem_path, wgs_dem_path, utm_dem_path):
     """
     Reproject raster from WGS to UTM projection.
@@ -871,8 +869,8 @@ def select_period(df, first, last):
         Clipped variable.
     """
     df = df[(df.index.year>=first) & (df.index.year<=last)]
-    return df 
-    
+    return df
+
 #%% PYHELP HELPER FUNCTIONS (migrated from pyhelp/helper.py)
 
 def load_csv(file_path: str) -> pd.DataFrame:
@@ -1024,7 +1022,7 @@ def convert_units(df: pd.DataFrame, var_key: str) -> pd.DataFrame:
         df = df * 1e-6
     return df
 
-#%% DISPLAY 
+#%% DISPLAY
 
 _banner_printed = False
 
@@ -1046,5 +1044,5 @@ def print_hydromodpy():
     for line in banner_lines:
         logger.info(line)
     _banner_printed = True
-    
+
 #%% NOTES
