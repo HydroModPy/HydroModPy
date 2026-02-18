9295e65b936a187d5f7f7b4084f803e5cb9420e2
Alex-Gauvain <alexandre.gauvain.ag@gmail.com>
2026-02-18 10:24:36 +0100

    add process module + abstract class solver

---
+++ hydromodpy/process/transport.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+class Transport(Process):
+    def __init__(self):
+        super().__init__()
+
+    def set_parameters(self, parameters: dict):
+        self.parameters.update(parameters)
+
+    def set_variables(self, variables: dict):
+        self.variables.update(variables)
+
+    def set_initial_conditions(self, initial_conditions: dict):
+        self.initial_conditions.update(initial_conditions)
+
+    def set_boundary_conditions(self, boundary_conditions: dict):
+        self.boundary_conditions.update(boundary_conditions)
+
+    def set_sinks_sources(self, sinks_sources: dict):
+        self.sinks_sources.update(sinks_sources)
+
