44b9105d7db117b86244549227fb291cad37d8e5
bastien-boivin <bastien.boivin@univ-rennes.fr>
2026-03-08 23:11:14 +0100

    [core] - Remove unused structure binder imports from simulation launcher

--- launchers/process_simulation/__init__.py
+++ launchers/process_simulation/__init__.py
@@ -1,19 +1,7 @@
 """Process simulation launcher workflow package."""
 
 from launchers.process_simulation.launcher import HydroModPyLauncher
-from hydromodpy.domain.structure_binders import (
-    apply_catchment_zones_to_domain,
-    apply_geology_to_domain,
-)
-from hydromodpy.process.flow.structure_binders import (
-    apply_climatic_to_flow_recharge,
-    apply_oceanic_to_flow,
-)
 
 __all__ = [
     "HydroModPyLauncher",
-    "apply_catchment_zones_to_domain",
-    "apply_geology_to_domain",
-    "apply_climatic_to_flow_recharge",
-    "apply_oceanic_to_flow",
 ]
