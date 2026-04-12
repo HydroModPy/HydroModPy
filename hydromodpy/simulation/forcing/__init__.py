a77f47aeab4f4973cbaa51dcb710782678fdae5a
Bastien Boivin <bastien.boivin@univ-rennes.fr>
2026-04-13 01:22:32 +0200

    [forcing] - remove unused constant from imports

--- hydromodpy/simulation/forcing/__init__.py
+++ hydromodpy/simulation/forcing/__init__.py
@@ -1,7 +1,6 @@
 """Re-export from hydromodpy.process.forcing."""
 
 from hydromodpy.process.forcing import (  # noqa: F401
-    _MM_PER_DAY_TO_M_PER_S,
     align_forcing_series_to_simulation_window,
     build_forcing_series,
     extract_homogeneous_series,
@@ -18,7 +17,6 @@ from hydromodpy.process.hydrology.synthetic.forcing import (
 )
 
 __all__ = [
-    "_MM_PER_DAY_TO_M_PER_S",
     "align_forcing_series_to_simulation_window",
     "build_forcing_series",
     "extract_homogeneous_series",
