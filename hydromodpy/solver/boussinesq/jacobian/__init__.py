5d1e29286ff9c81625758dedbdd667e26d191630
Jean-Raynald de Dreuzy <Jean-Raynald.de-Dreuzy@univ-rennes1.fr>
2026-04-17 02:07:23 +0200

    Document gallery manifest workflow and jacobian exports

--- hydromodpy/solver/boussinesq/jacobian/__init__.py
+++ hydromodpy/solver/boussinesq/jacobian/__init__.py
@@ -1,9 +1,11 @@
 """Jacobian builders and sparse operator helpers for the Boussinesq solver."""
 
 from hydromodpy.solver.boussinesq.jacobian.fd import (
+    build_colored_sparse_fd_jacobian_triplets,
     build_dense_fd_jacobian,
+    build_cell_coupling_rows_by_column,
     build_sparse_fd_jacobian_triplets,
-    greedy_column_coloring,
+    color_columns_by_row_overlap,
 )
 from hydromodpy.solver.boussinesq.jacobian.semianalytic import (
     build_sparse_semianalytic_base_jacobian_triplets,
@@ -12,8 +14,10 @@ from hydromodpy.solver.boussinesq.jacobian.semianalytic import (
 
 __all__ = [
     "build_dense_fd_jacobian",
+    "build_cell_coupling_rows_by_column",
+    "build_colored_sparse_fd_jacobian_triplets",
     "build_sparse_fd_jacobian_triplets",
     "build_sparse_semianalytic_base_jacobian_triplets",
     "build_sparse_semianalytic_partition_jacobian_triplets",
-    "greedy_column_coloring",
+    "color_columns_by_row_overlap",
 ]
