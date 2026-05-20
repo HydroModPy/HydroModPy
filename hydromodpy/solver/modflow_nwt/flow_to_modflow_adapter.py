55796627a0b84e5dab48705d4c29a359746910f9
Bastien Boivin <bastien.boivin@proton.me>
2026-05-20 15:46:18 +0200

    cleanup(solver): drop flow_to_modflow_adapter alias module

--- hydromodpy/solver/modflow_nwt/flow_to_modflow_adapter.py
+++
@@ -1,5 +0,0 @@
-"""Public import path for Flow -> MODFLOW adapter types."""
-
-from .nwt.flow_to_modflow_adapter import FlowModflowInputs, FlowToModflowAdapter
-
-__all__ = ["FlowModflowInputs", "FlowToModflowAdapter"]
