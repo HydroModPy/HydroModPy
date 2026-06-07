346537595cae2bdf3fb3f2f3468a9156ad6882b5
Jean-Raynald de Dreuzy <Jean-Raynald.de-Dreuzy@univ-rennes1.fr>
2026-06-07 23:33:26 +0200

    fix: expose validity frame package exports

---
+++ validity_frame/src/validity_frame/auto_capture/__init__.py
@@ -0,0 +1,6 @@
+"""Automatic runtime capture helpers for validity frames."""
+
+from .collector import AutoCaptureCollector
+from .context import ExecutionContext
+
+__all__ = ["AutoCaptureCollector", "ExecutionContext"]
